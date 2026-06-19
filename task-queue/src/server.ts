37d2847148bab6a590e16b32da402b7999072167
PRAVEEN KUMAR <praveen.iyu123@gmail.com>
2026-06-19 15:20:44 +0530

    fix: remove debug console.log and fix prettier formatting in server.ts

--- task-queue/src/server.ts
+++ task-queue/src/server.ts
@@ -34,5 +34,5 @@ function startServer(): void {
   process.on('SIGTERM', () => handleShutdown('SIGTERM'));
   process.on('SIGINT', () => handleShutdown('SIGINT'));
 }
-console.log('Is everything ok !');
+
 startServer();
